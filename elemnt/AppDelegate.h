0fe5c323344f447a02ecba38410a5aff11a3150f
Pankaj Sharma <preeti@Preetis-Mac-mini.local>
2014-09-29 22:37:26 +0530

    Initial Commit

---
+++ elemnt/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  elemnt
+//
+//  Created by Pankaj Sharma on 9/29/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
